a85d25306bbabe727079b99040cc9b3503f639f8
주현성 <joo@juhyeonseong-ui-MacBookPro.local>
2024-11-24 00:40:10 +0900

    add header bar

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue'; // App.vue 연결
+import './index.css'; // Tailwind 연결
+import "@fortawesome/fontawesome-free/css/all.css";
 
-createApp(App).mount('#app')
+
+createApp(App).mount('#app'); // #app에 마운트
